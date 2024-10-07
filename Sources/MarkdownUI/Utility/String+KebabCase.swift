bdbb9a7c9138c232560de5e1dbc6c082340cbec5
Rakuraku Jyo <352545+rjyo@users.noreply.github.com>
2024-10-07 13:42:41 +0900

    Update String+KebabCase.swift

--- Sources/MarkdownUI/Utility/String+KebabCase.swift
+++ Sources/MarkdownUI/Utility/String+KebabCase.swift
@@ -7,3 +7,39 @@ extension String {
       .joined(separator: "-")
   }
 }
+
+
+public extension String {
+  func toAttributedString() -> AttributedString {
+    let blocks = [BlockNode](markdown: self)
+
+    let baseURL: URL? = nil
+    let softBreakMode: SoftBreak.Mode = .space
+    let attributes = AttributeContainer()
+    let myTheme = Theme()
+
+    let attributedString = blocks.reduce(into: AttributedString()) { result, block in
+      switch block {
+      case .paragraph(let inlineNodes):
+        for node in inlineNodes {
+          result += node.renderAttributedString(
+            baseURL: baseURL,
+            textStyles: InlineTextStyles(
+              code: myTheme.code,
+              emphasis: myTheme.emphasis,
+              strong: myTheme.strong,
+              strikethrough: myTheme.strikethrough,
+              link: myTheme.link
+            ),
+            softBreakMode: softBreakMode,
+            attributes: attributes
+          )
+        }
+        result += AttributedString("\n\n")
+      default:
+        break
+      }
+    }
+    return attributedString
+  }
+}
